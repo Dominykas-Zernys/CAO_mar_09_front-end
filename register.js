702ac2f3836391a0cb1ff7fe48a482291372885d
Dominykas-Zernys <zernysdominykas@gmail.com>
2022-03-09 23:25:18 +0200

    init commit

---
+++ register.js
@@ -0,0 +1,7 @@
+const registerForm = document.getElementById('register-form');
+const emailInput = document.getElementById('email-input');
+const passwordInput = document.getElementById('password-input');
+
+registerForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
