b34af393e72f3c2e928441831795414a0d926e71
Dominykas-Zernys <zernysdominykas@gmail.com>
2022-03-09 23:58:33 +0200

    register done

--- register.js
+++ register.js
@@ -1,7 +1,32 @@
 const registerForm = document.getElementById('register-form');
 const emailInput = document.getElementById('email-input');
 const passwordInput = document.getElementById('password-input');
+const errorMessage = document.getElementById('error-message');
+const allMainElements = document.querySelectorAll('main');
 
 registerForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  const objToSend = { email: emailInput.value, password: passwordInput.value };
+  registerUser(objToSend);
 });
+
+async function registerUser(newUserObj) {
+  try {
+    const res = await fetch('http://localhost:3000/users/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newUserObj),
+    });
+    const response = await res.json();
+    if (!response.success) {
+      errorMessage.textContent = 'Could not create user';
+      setTimeout(() => {
+        errorMessage.textContent = '';
+      }, 5000);
+      return;
+    }
+    allMainElements.forEach((main) => {
+      main.classList.toggle('hidden');
+    });
+  } catch (error) {}
+}
